314e0b97abcb9a5c0353343f44974b41d30e431f
Yuji181181 <s1f102402552@iniad.org>
2024-12-29 01:36:23 +0900

    パラメータ

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -6,3 +7,20 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
+
+
+@app.get("/countries/{country_name}")
+def get_country(country_name: str):
+    return {"country_name": country_name}
+
+
+# クエリパラメーター
+@app.get("/countries/")  # 引数を書かない
+def get_country(country_name: str = "japan"):  # デフォルト値を設定
+    return {"country_name": country_name}
+
+
+# オプショナルパラメーター
+@app.get("/countries/")
+def get_country(country_name: Optional[str] = None):  # 必須ではない設定にする方法
+    return {"country_name": country_name}
